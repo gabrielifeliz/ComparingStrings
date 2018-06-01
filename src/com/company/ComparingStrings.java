052fdb6c6ae1df8c536fc99065db7897af11b049
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-06-01 09:23:55 -0400

    initial commit

---
+++ src/com/company/ComparingStrings.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ComparingStrings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How is the weather?");
+        String answer = sc.nextLine();
+
+        /*
+         * Change equal signs (==) to .equals() method to compare strings so that the
+         * program works successfully
+         */
+        /*
+         * The answer option "stormy" and its output "You should stay inside" is added
+         */
+
+        if (answer.equals("rain"))
+            System.out.println("Take your umbrella!");
+        else if (answer.equals("windy"))
+            System.out.println("Wear your jacket!");
+        else if (answer.equals("snow"))
+            System.out.println("Wear a coat and take a shovel!");
+        else if (answer.equals("stormy"))
+            System.out.println("You should stay inside");
+        else
+            System.out.println("Enjoy your day!");
+
+        sc.close();
+    }
+}
